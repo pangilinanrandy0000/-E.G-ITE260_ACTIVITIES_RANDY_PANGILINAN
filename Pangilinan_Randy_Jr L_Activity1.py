c44198fefe55510fe0f2c48fb8d9e5f5622bb046
pangilinanrandy0000 <pangilinanrandy781@gmail.com>
2026-07-27 16:47:34 +0800

    Add files via upload

---
+++ Pangilinan_Randy_Jr L_Activity1.py
@@ -0,0 +1,9 @@
+===== STUDENT INFORMATION =====
+Name:Randy L Pangilinan Jr
+Age: 18
+Course: BS Information Technology
+Section: BSIT1-S10
+Address: Baloc, Sto, Domingo Nueva Ecija
+Favorite Hobby: Playing online games and Playing Guitar,Bass,Drums
+
+[Program finished]
---
+++ Pangilinan_Randy_Jr L_Activity2.py
@@ -0,0 +1,9 @@
+===== USER PROFILE =====
+Name: Randy L Pangilinan Jr
+Age: 18
+Course: BS Information Technology
+Section: BSIT1-S10
+Address: Baloc, Santo Domingo, Nueva Ecija
+Favorite Food: Kare Kare, Shawarma,
+
+[Program finished]
